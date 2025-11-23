e3f809dfacd502396fde536dbc145a6b452d7657
Joakim Nyman <joakim@bitwolfe.io>
2025-11-24 01:41:03 +0200

    Start of Neovim setup

---
+++ neovim/.config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        automatic_installation = true
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.enable('lua_ls')
+      vim.lsp.enable('ts_ls')
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
