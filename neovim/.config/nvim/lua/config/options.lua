7963c03bdfd2411d5f49b1e745c57a4fe3f4ce9d
Joakim Nyman <joakim@bitwolfe.io>
2025-12-02 11:26:29 +0200

    Nvim: Re-enable relative line numbers

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 vim.opt.scrolloff = 6
 vim.g.root_spec = { "cwd" } -- don't want the root dir to change
