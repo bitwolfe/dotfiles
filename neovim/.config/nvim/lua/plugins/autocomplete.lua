25811a0580d97d02db05cbf5f5b27e7d312bb12c
Joakim Nyman <joakim@bitwolfe.io>
2025-12-11 16:55:15 +0200

    Nvim: Add blink recipe for dynamic positioning

--- neovim/.config/nvim/lua/plugins/autocomplete.lua
+++ neovim/.config/nvim/lua/plugins/autocomplete.lua
@@ -19,6 +19,7 @@ return {
         },
         menu = {
           border = "rounded",
+          -- Colorful-menu integration
           draw = {
             columns = { { "kind_icon" }, { "label", gap = 1 } },
             components = {
@@ -32,6 +33,25 @@ return {
               },
             },
           },
+          -- Dynamic positioning to not overlap ghost text
+          direction_priority = function()
+            local ctx = require("blink.cmp").get_context()
+            local item = require("blink.cmp").get_selected_item()
+            if ctx == nil or item == nil then
+              return { "s", "n" }
+            end
+
+            local item_text = item.textEdit ~= nil and item.textEdit.newText or item.insertText or item.label
+            local is_multi_line = item_text:find("\n") ~= nil
+
+            -- after showing the menu upwards, we want to maintain that direction
+            -- until we re-open the menu, so store the context id in a global variable
+            if is_multi_line or vim.g.blink_cmp_upwards_ctx_id == ctx.id then
+              vim.g.blink_cmp_upwards_ctx_id = ctx.id
+              return { "n", "s" }
+            end
+            return { "s", "n" }
+          end,
         },
         signature = {
           window = {
