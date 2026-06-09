d0f419b73ef973d685378842f4510d34b180e814
Joakim Nyman <joakim@bitwolfe.io>
2026-06-09 13:50:00 +0300

    Attempt to unbreak LazyVim formatting

--- neovim/.config/nvim/lua/plugins/conform.lua
+++ neovim/.config/nvim/lua/plugins/conform.lua
@@ -1,10 +1,31 @@
 return {
   {
     "stevearc/conform.nvim",
-    opts = {
-      default_format_opts = {
-        timeout_ms = 3000,
-      },
-    },
+    opts = function()
+      local config = { "prettier", stop_after_first = true }
+
+      local opts = {
+        default_format_opts = {
+          timeout_ms = 3000,
+        },
+        formatters_by_ft = {
+          -- Use a sub-list to run only the first available formatter
+          javascript = config,
+          typescript = config,
+          ["javascript.glimmer"] = config,
+          ["typescript.glimmer"] = config,
+        },
+        formatters = {
+          -- We don't want to enable prettierd
+          -- because it requires global installation, and then
+          -- we can't even have projects without prettier
+          -- NOTE: make sure prettier (and prettierd) are not installed globally
+          prettier = {
+            require_cwd = true,
+          },
+        },
+      }
+      return opts
+    end
   },
 }
