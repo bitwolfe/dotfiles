5620c940dc757158a1dd101aa1e4a59ca63e5ef0
Joakim Nyman <joakim@bitwolfe.io>
2026-08-24 17:32:06 +0300

    Nvim: Configure markdown linting rules

--- neovim/.config/nvim/lua/plugins/markdown.lua
+++ neovim/.config/nvim/lua/plugins/markdown.lua
@@ -37,4 +37,15 @@ return {
       },
     },
   },
+  -- Make the markdown linter follow some global rules
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          prepend_args = { "--config", vim.fn.stdpath("config") .. "/.markdownlint.yaml", "--" },
+        },
+      },
+    },
+  },
 }
